3093dfcc5198831e968ad607923ade3b0dfe9d3e
MunhozTheMage <gmnobrega0704@gmail.com>
2020-09-03 11:24:28 -0300

    Updates

---
+++ precache-manifest.23c6eaaf723d58d144c2a191ff4d2409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca3889cf88c69150c68efc1fe987c09",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "17e77f29de2f21803878",
+    "url": "/pokedex/static/css/main.1df8d0fa.chunk.css"
+  },
+  {
+    "revision": "5ff312b4ac299a0703b4",
+    "url": "/pokedex/static/js/2.4dc058d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.4dc058d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e77f29de2f21803878",
+    "url": "/pokedex/static/js/main.2c3e7b4c.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
